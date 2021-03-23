5081332e32f9cb15579ac6d0b276998207a385ad
“Tariq <2482879a@student.gla.ac.uk>
2021-03-23 15:59:48 +0300

    Completed Index Page

--- library/models.py
+++ library/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    
+class Book(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
