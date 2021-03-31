bef8f75c10ddd7886a64e77a375197e8ed97ade9
“Tariq <2482879a@student.gla.ac.uk>
2021-03-31 23:46:58 +0300

    added models

---
+++ populate_library.py
@@ -0,0 +1,72 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'group_project.settings')
+
+
+import django
+django.setup()
+from library.models import Genre,Book
+
+
+def populate():
+    # First, we will create lists of dictionaries containing the pages
+    # we want to add into each category.
+    # Then we will create a dictionary of dictionaries for our categories.
+    # This might seem a little bit confusing, but it allows us to iterate
+    # through each data structure, and add the data to our models.
+    
+    fiction_books = [
+        {'title': 'Book Fiction 1',
+         'url':'http://docs.python.org/3/tutorial/'},
+        {'title':'Book Fiction 2',
+         'url':'http://www.greenteapress.com/thinkpython/'},
+        {'title':'Book Fiction 3',
+         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+    
+    nonfiction_books = [
+         {'title':'Book NonFiction 1',
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        {'title':'Book NonFiction 2',
+         'url':'http://www.djangorocks.com/'},
+        {'title':'Book NonFiction 3',
+         'url':'http://www.tangowithdjango.com/'} ]
+    
+    children_books = [
+        {'title':'Book Children 1',
+         'url':'http://bottlepy.org/docs/dev/'},
+        {'title':'Book Children 2',
+         'url':'http://flask.pocoo.org'} ]
+    
+    cats = {'Fiction': {'books': fiction_books},
+            'Nonfiction': {'books': nonfiction_books},
+            'Children': {'books': children_books} }
+    
+    # If you want to add more categories or pages,
+    # add them to the dictionaries above.
+    # The code below goes through the cats dictionary, then adds each category
+     # and then adds all the associated pages for that category.
+    for cat, cat_data in cats.items():
+        c = add_genre(cat)
+        for p in cat_data['books']:
+            add_book(c, p['title'], p['url'])
+
+    # Print out the categories we have added.
+    for c in Genre.objects.all():
+        for p in Book.objects.filter(genre=c):
+            print(f'- {c}: {p}')
+
+def add_book(cat,title,url,views=0):
+    p = Book.objects.get_or_create(genre=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
+
+def add_genre(name):
+    c = Genre.objects.get_or_create(name=name)[0]
+    c.save()
+    return c
+
+#Startexecutionhere!
+if __name__=='__main__':
+    print('Starting Rango population script...')
+    populate()
